fd8a0841a67b25719fb511bfc20e4c5781f03faa
Calix0807 <calixkylesionson16@gmail.com>
2025-11-03 17:34:48 +0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -266,7 +266,7 @@ function formatTimeTo12Hour(timeStr) {
       // const crs   = all.filter(r => r.type === "cr");
       try {
           // ✅ Call your Flask API to get live rooms
-          const res = await fetch("http://localhost:5000/api/v1/rooms/");
+          const res = await fetch("https://python-ss4m.onrender.com/api/v1/rooms/");
           const allRooms = await res.json();
 
           // ✅ Filter by parent building
@@ -334,7 +334,7 @@ function formatTimeTo12Hour(timeStr) {
           panel.style.display = 'block';
           panel.innerHTML = `<p>Loading...</p>`;
           try {
-            const res = await fetch(`http://localhost:5000/api/v1/schedules/${tag}`);
+            const res = await fetch(`https://python-ss4m.onrender.com/api/v1/schedules/${tag}`);
             if (!res.ok) throw new Error(`Schedules not found for ${tag}`);
             const schedules = await res.json();
             panel.innerHTML = renderSchedTable(schedules);
